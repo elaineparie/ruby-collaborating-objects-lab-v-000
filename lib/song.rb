3a380d9022c3a0daca1e71adfc9217bcab43dbe5
Elaine Anderson <eparie@bu.edu>
2018-06-05 13:51:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,11 +7,10 @@ class Song
   end
 
 def self.new_by_filename(filename)
-  binding.pry
-new_song = Song.new(name)
 formatted_song = filename.split('-')
+new_song = Song.new(formatted_song[1]) 
 new_song.artist_name = formatted_song[0]
-new_song.artist = formatted_song[1]
+#new_song.artist = formatted_song[1]
 new_song
 end
 
