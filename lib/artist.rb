1ad0739b024ecf62c61608ef81c039e30b3a33bf
Elaine Anderson <eparie@bu.edu>
2018-06-05 13:28:23 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,55 @@
+
+
+class Artist
+
+  attr_accessor :name, :songs
+
+    @songs = []
+    @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+  @@all
+  end
+
+def add_song(song)
+  @songs << song
+  song.artist = self
+end
+
+def print_songs
+  @songs
+end
+
+def self.create_by_name(name)
+  artist = self.new(name)
+  artist.name = name
+  artist.save
+  artist
+end
+
+def self.find_by_name(name)
+  self.all.detect{|artist| artist.name == name}
+end
+
+def self.find_or_create_by_name(name)
+#song = self.new
+  if self.find_by_name(name)
+    self.find_by_name(name)
+  else
+    self.create_by_name(name)
+      end
+    end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist, :artist_name
+
+  def initialize(name)
+    @name = name
+  end
+
+def self.new_by_filename(filename)
+formatted_song = Song.new(name)
+formatted_song = filename.split('-')
+formatted_song.artist_name = formatted_song[1]
+formatted_song.artist = formatted_song[2]
+formatted_song
+end
+
+def artist_name(artist_name)
+  @artist_name = Artist.find_or_create_by_name
+  add_song(artist_name)
+end
+
+end
